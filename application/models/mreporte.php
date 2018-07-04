9d96a9a6815155b69869bdff92d3050290a36699
juan1312 <juan1312995@gmail.com>
2018-07-03 21:57:57 -0400

    a

--- application/models/mreporte.php
+++ application/models/mreporte.php
@@ -1,18 +1,28 @@
 <?php  
 	defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Musuario extends CI_Model 
+class Mreporte extends CI_Model 
   {
 
   		function __construct()
 		{
 			parent::__construct();
 			$this->load->library('session');
-
 		}
 		
 		
-		
+		public function guardar($parametro)
+	    {
+	    	$campos = array(
+					'asunto'		    => $parametro['asunto'],
+					'descripcion'	    => $parametro['descripcion'],
+					'usuario'  		    => $parametro['FK_descripcion'],
+					
+					 );
+			$this->db->insert('reporte',$campos);
+
+	   		return $this->db->insert_id();
+	    }
 		
 	}
 
